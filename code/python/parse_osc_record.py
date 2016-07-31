119ebe4baec76a4f7f2a09835a885a3df9cdfdbb
Steven A. Bjornson <info@sabjorn.net>
2016-07-31 10:53:25 -0700

    added more metrics for analysis of data record tests

--- code/python/parse_osc_record.py
+++ code/python/parse_osc_record.py
@@ -1,22 +1,46 @@
 #a script to automatically parse an OSC csv recording
+import argparse
 import numpy as np
 
-filename = 'datarecord_31-07-16-1469949684_0e2b0_10_r0g0b0.csv'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str, 
+                        help='the name of the csv file to be loaded.')
+    
+    args = parser.parse_args()
 
-data = np.genfromtxt(filename, delimiter=',', names=True)
+    data = np.genfromtxt(args.filename, delimiter=',', names=True)
 
-#find total length of time
-total_time = (data['time'][-1] - data['time'][0]) / 1000. / 60. / 60.
-print total_time
+    #find total length of time
+    total_time = (data['time'][-1] - data['time'][0]) / 1000. / 60. / 60.
+    print total_time
 
-#normalizae battery 
-max_batt = np.max(data['batt'])
-min_batt = np.min(data['batt'])
-data['batt'] = (data['batt'] - min_batt) / (max_batt - min_batt)
+    #normalizae battery 
+    max_batt = np.max(data['batt'])
+    min_batt = np.min(data['batt'])
+    data['batt'] = (data['batt'] - min_batt) / (max_batt - min_batt)
 
-#time between frames
-time_diff = np.diff(data['time'])
-mean_time = np.mean(time_diff)
-median_time = np.median(time_diff)
-max_time = np.max(time_diff)
-min_time = np.min(time_diff)
+    #time between frames
+    time_diff = np.diff(data['time'])
+    mean_time = np.mean(time_diff)
+    median_time = np.median(time_diff)
+    max_time = np.max(time_diff)
+    min_time = np.min(time_diff)
+
+    time_hist = np.bincount(time_diff.astype('int64'))
+    largest_val = np.argmax(time_hist)
+    time_error_hist =  time_hist / float(len(time_diff)) #the error
+
+    #how many packets lost
+    packet_diff = np.diff(data['packet'])
+    max_pack = np.max(packet_diff)
+    min_pack = np.min(packet_diff)
+    mean_pack = np.mean(packet_diff)
+    median_pack = np.median(packet_diff)
+
+    #compute the packet error (% of packets lost)
+    error = np.sum(packet_diff > 1.) / float(len(packet_diff))
+
+    packet_hist = np.bincount(packet_diff.astype('int64'))
+    packet_largest_val = np.argmax(packet_hist)
+    packet_error_hist =  packet_hist / float(len(packet_diff)) #the error
